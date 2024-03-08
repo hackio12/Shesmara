a693a036cea941a50fc5aff0263a5576e8de6b54
hackio12 <shreyaspai12005@gmail.com>
2024-03-08 12:02:52 +0530

    update the haeder and make the profile

--- SHESMARA/client/src/App.jsx
+++ SHESMARA/client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 
@@ -17,7 +18,9 @@ export default function App() {
       <Route path="/sign-In"  element={<Signin />}/>
       <Route path="/sign-up"  element={<Signup />}/>
       <Route path="/about"  element={<About />}/>
+      <Route  element={<PrivateRoute />}>
       <Route path="/profile"  element={<Profile />}/>
+      </Route>
     </Routes>
     </BrowserRouter>
   )

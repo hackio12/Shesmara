d95c5687c0a4c624bbe9a1db6711af66ac4bbeaa
hackio12 <shreyaspai12005@gmail.com>
2024-03-13 10:26:25 +0530

    complete of sign in ui

--- SHESMARA/client/src/pages/Signin.jsx
+++ SHESMARA/client/src/pages/Signin.jsx
@@ -1,71 +1,107 @@
-import { useState } from 'react'
-import {Link, useNavigate} from 'react-router-dom'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { signInStart, signInSuccess, signInFailure} from '../redux/user/userSlice';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 import OAuth from '../components/OAuth';
 
-
 export default function Signin() {
-  const [formData, setFormData]= useState({})
-  const {loading, error}= useSelector((state)=> state.user);
-  const navigate=  useNavigate();
-  const dispatch= useDispatch();
-  const handleChange = (e) =>{
-    setFormData(
-      {
-        ...formData,
-        [e.target.id]: e.target.value,
-      }
-    );
-  
-  };
-  const handleSubmit= async (e)=>{
-    e.preventDefault();
-    if (!formData.email || !formData.password ){
-      return
-    }
-    try{
-      dispatch(signInStart());
-    const res= await fetch ('/api/auth/signin',{
-      method:'POST',
-      headers:{
-        'Content-Type':'application/json',
-      },
-      body: JSON.stringify(formData),
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
     });
-    const data =await res.json();
-    console.log(data);
-    if (data.success === false){
-      dispatch(signInFailure(data.message));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
       return;
     }
-    dispatch(signInSuccess(data));
-    navigate('/')
-    } catch (error){
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
       dispatch(signInFailure(error.message));
-
     }
-
   };
+
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
-        <input type="email" placeholder="email" className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type="password" placeholder="password" className='border p-3 rounded-lg' id='password'onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 diasabled- opacity-80'>
-          {loading ? 'loading...': 'Sign In'}
-        </button>
-        <OAuth/>
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Dont have an account?</p>
-        <Link to={"/sign-up"}>
-          <span className='text-blue-700'>Sign up </span>
-        </Link>
+    <div className="flex items-center justify-center w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg lg:max-w-4xl">
+      <div className="hidden bg-cover lg:block lg:w-1/2" style={{ backgroundImage: "url('')" }}></div>
+
+      <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+        <div className="flex justify-center mx-auto">
+          <img className="w-auto h-7 sm:h-8" src="" alt="" />
+        </div>
+
+        <p className="mt-3 text-xl text-center uppercase text-slate-800">
+          Welcome back!
+        </p>
+        <a href="#" className="flex items-center justify-center mt-4 text-balck-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-black-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <div className="px-4 py-2">
+                <svg className="w-6 h-6" viewBox="0 0 40 40">
+                    <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
+                    <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
+                    <path d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z" fill="#4CAF50" />
+                    <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
+                </svg>
+                <div className='w-5/6 px-4 py-3 font-bold text-center'><OAuth /></div>
+            </div>
+            </a>
+
+            <div className="flex items-center justify-between mt-4">
+            <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
+            <div className="text-xs text-center text-black uppercase dark:text-black hover:underline">or login with email</div>
+
+            <span className="w-1/5 border-b border-gray-400 lg:w-1/4"></span>
+        </div>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
+        <div className="mt-4">
+            <label className="block mb-2 text-sm font-medium text-black-600  text-black-200" for="LoggingEmailAddress">Email Address</label>
+            <input id="email" className="block w-full px-4 py-2 text-black-700 bg-white border rounded-lg bg-black-800 text-black-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="email" onchange={handleChange} />
+        </div>
+        <div className="mt-4">
+        <label className="block mb-2 text-sm font-medium text-balck-600  text-black-200" for="loggingPassword">Password</label>
+            <input id="password" className="block w-full px-4 py-2 text-black-700 bg-white border rounded-lg bg-black-800 text-black-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="password" onchange={handleChange} />
+            <div className="flex justify-end">
+            <a href="#" className="text-xs text-blue-500 text-black-300 hover:underline">Forget Password?</a>
+            </div>
+        </div>
+        <div class="mt-6">
+            <button class="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">{loading ? 'Loading...' : 'Sign In'}
+            </button>
+        </div>
+        </form>
+
+        <div class="flex items-center justify-center mt-4">
+    <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+    <Link to={"/sign-up"} className="flex items-center">
+        <span class="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline">or sign up</span>
+    </Link>
+    <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+</div>
+
+        {error && <p className='text-red-500 mt-5'>{error}</p>}
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }

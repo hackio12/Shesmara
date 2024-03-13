45f8f2bf2dde70c8133a839a71ca4ddaaf5e90a9
hackio12 <shreyaspai12005@gmail.com>
2024-03-13 21:21:14 +0530

    completed sign up page ui

--- SHESMARA/client/src/pages/Signup.jsx
+++ SHESMARA/client/src/pages/Signup.jsx
@@ -46,24 +46,69 @@ export default function Signup() {
 
   };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
-        <input type="text" placeholder="username" className='border p-3 rounded-lg' id='username' onChange={handleChange} />
-        <input type="email" placeholder="email" className='border p-3 rounded-lg' id='email' onChange={handleChange} />
-        <input type="password" placeholder="password" className='border p-3 rounded-lg' id='password'onChange={handleChange} />
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 diasabled- opacity-80'>
-          {loading ? 'loading...': 'Sign Up'}
-        </button>
-        <OAuth/>
-      </form>
-      <div className='flex gap-2 mt-5'>
+    <div className="flex items-center justify-center w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg lg:max-w-4xl">
+      <div className="hidden bg-cover lg:block lg:w-1" style={{ backgroundImage: "url('')" }}></div>
+
+      <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+        <div className="flex justify-center mx-auto">
+          <img className="w-auto h-7 sm:h-8" src="{}" alt="" />
+        </div>
+
+        <p className="mt-3 text-xl text-center uppercase text-slate-800">
+          Sign up 
+        </p>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4 '>
+        <div class="relative flex items-center mt-4">
+                <span class="absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-black-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+                </span>
+                <input type="text" className="block w-full py-3 text-black-700 bg-white border rounded-lg px-11 dark:bg-white-900 dark:text-black-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="username" onchange={handleChange}/>
+            </div>
+            <div class="relative flex items-center mt-4">
+                <span class="absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-black-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                </span>
+                <input type="email" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-white-900 dark:text-black-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address" onchange={handleChange}/>
+            </div>
+
+         <div class="relative flex items-center mt-4">
+                <span class="absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-3 text-gray-300 dark:text-black-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                    </svg>
+                </span>
+                <input type="password" class="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-white-900 dark:text-balck-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password" onChange={handleChange}/>
+            </div>
+            
+        <div class="mt-4">
+            <button class="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">{loading ? 'Loading...' : 'Sign In'}
+            </button>
+        </div>
+        <a href="#" className="flex items-center justify-center mt-1  text-balck-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-black-200 hover:bg-gray-50 dark:hover:bg-gray-600 ">
+            <div className="px-3 py-0.2">
+                <svg className="w-6 h-6" viewBox="0 0 40 40">
+                    <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
+                    <path d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z" fill="#FF3D00" />
+                    <path d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z" fill="#4CAF50" />
+                    <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
+                </svg>
+                
+            </div>
+            <span class="w-5/6 px-4 py-3 font-bold text-center"><OAuth/></span>
+            </a>
+        </form>
+        <div className='flex items-center justify-center mt-4'>
         <p>Have an account?</p>
-        <Link to={"/sign-in"}>
-          <span className='text-blue-700'>Sign in </span>
-        </Link>
+        <Link to={"/sign-in"} className="flex justify-center items-center">
+        <span class="text-xs text-center text-blue-500 uppercasehover:underline">Sign in!</span>
+    </Link>
+</div>
+        {error && <p className='text-red-500 mt-5'>{error}</p>}
       </div>
-      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
-  )
+  );
 }
